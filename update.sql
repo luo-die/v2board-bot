9155b208b3efd271274941617b418868267a8985
落天 <62587756+luo-die@users.noreply.github.com>
2021-09-27 22:51:21 +0800

    修复
    修复

--- update.sql
+++ update.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `v2_user`
 ADD `last_checkin_at` int(11) NOT NULL DEFAULT '0' AFTER `transfer_enable`;
+ALTER TABLE `v2_user`
 ADD `last_lucky_at` int(11) NOT NULL DEFAULT '0' AFTER `transfer_enable`;
